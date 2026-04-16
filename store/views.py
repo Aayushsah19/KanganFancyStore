1e18e73c19c60948c499579884dee9f170c360fa
Aayush Sah <aayushsah@Aayushs-MacBook-Air.local>
2026-04-16 11:04:36 +0545

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def home(request):
+    # Ye logic automatic 22 items ki list bana dega
+    products = []
+    for i in range(1, 23):  # 1 se 22 tak chalega
+        item = {
+            'id': i,
+            'name': f'Fancy Kangan Design #{i}',
+            'price': 1000 + (i * 150),  # Har item ka price thoda alag dikhega
+            'image': f'item{i}.jpg'     # Ye aapki photo ka naam dhoondega
+        }
+        products.append(item)
+    
+    context = {
+        'items': products,
+        'store_name': 'Kangan Fancy Store',
+        'location': 'Siraha, Nepal'
+    }
+    return render(request, 'index.html', context)
